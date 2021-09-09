a0aafaf6b2fe50c1559573df119549255b8e5bfe
procoder4 <65418744+procoder4@users.noreply.github.com>
2021-09-09 15:50:42 +0300

    added animated cursor

--- src/views/all-home-version/HomeDark.jsx
+++ src/views/all-home-version/HomeDark.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import HomeLight from "../all-home-version/HomeLight";
+import AnimatedCursor from "react-animated-cursor"
+
 
 const HomeDark = () => {
   document.body.classList.add("dark");
   return (
     <>
       <HomeLight />
+      <div className="App">
+    <AnimatedCursor
+      innerSize={8}
+      outerSize={8}
+      color='255, 255, 255'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+    />
+    </div>
       
     </>
   );
